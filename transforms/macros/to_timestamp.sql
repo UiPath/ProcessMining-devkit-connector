9b13a55df13b1663ae2d100a5f97429bd7aed783
Celine Verhoef <celine.verhoef@uipath.com>
2021-10-19 18:06:46 +0200

    fix: updated format for Snowflake

--- transforms/macros/to_timestamp.sql
+++ transforms/macros/to_timestamp.sql
@@ -2,7 +2,7 @@
 
 -- Timestamp format: YYYY-MM-DD hh:mm:ss
 {% if var("database") == 'snowflake' %}
-    try_to_timestamp({{ attribute }}, 'YYYY-MM-DD hh24:mi:ss')
+    try_to_timestamp({{ attribute }}, 'YYYY-MM-DD hh24:mi:ss.ff3')
 {% elif var("database") == 'sqlserver' %}
     try_convert(datetime, {{ attribute }}, 20)
 {% endif %}
