52a6642a03ccbdeceb87a2ca8d8a15a365fd217b
Celine Verhoef <celine.verhoef@uipath.com>
2021-10-19 17:58:31 +0200

    feat: updated the macros
    Fixed default time and date format

--- transformations/macros/to_time.sql
+++ transformations/macros/to_time.sql
@@ -1,10 +1,10 @@
 {% macro to_time(attribute) %}
 
+-- Time format: hh:mm:ss
 {% if var("database") == 'snowflake' %}
-    try_to_time({{ attribute }}, 'HH24MISS')
+    try_to_time({{ attribute }}, 'hh24:mi:ss')
 {% elif var("database") == 'sqlserver' %}
-    -- SQL server does not support converting 6 digits time format to time data type, use custom conversion.
-    try_convert(time, concat(substring({{ attribute }}, 1, 2), ':', substring({{ attribute }}, 3, 2), ':', substring({{ attribute }}, 5, 2)), 8)
+    try_convert(time, {{ attribute }}, 8)
 {% endif %}
 
 {% endmacro %}
