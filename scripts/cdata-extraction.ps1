bcf5d33e6671dfa2b840e0fe633696319810be73
Silviu stanimir <silviu.stanimir@uipath.com>
2021-11-16 11:27:30 +0100

    docs: fixed the comment symbols in the powershell extraction script

--- scripts/cdata-extraction.ps1
+++ scripts/cdata-extraction.ps1
@@ -1,4 +1,4 @@
-// Configure the variables for the extraction
+<# Configure the variables for the extraction #>
 $URL='YOUR_CDATA_SERVER_URL'
 $authtoken='YOUR_AUTHENTICATION_TOKEN'
 $job='YOUR_JOB'
@@ -8,16 +8,16 @@ $params = @{"JobName"=$job;
  "Async"="false";
 }
 
-// Execute the extraction
+<# Execute the extraction #>
 echo "Sending POST request"
 Invoke-WebRequest -Headers @{'x-cdata-authtoken'=$authtoken} -Method POST -Body ($params|ConvertTo-Json) -ContentType "application/json" -Uri $URL/api.rsc/executeJob -OutFile executeJob.txt
 echo "Finished sending POST request"
 
-// Save the POST request response
+<# Save the POST request response #>
 $json = Get-Content 'executeJob.txt' | Out-String | ConvertFrom-Json
 
 $successfulExtraction = $false
-// Insert your logic here to check the status of the extraction
+<# Insert your logic here to check the status of the extraction #>
 foreach($element in $json.value)
 {
     if($element.Status -eq "SUCCESS" -And $element.Result -eq $null)
@@ -26,13 +26,13 @@ foreach($element in $json.value)
     }
 }
 
-// If the extraction is successful
+<# If the extraction is successful #>
 if($successfulExtraction -eq $true)
 {
     echo "Successful extraction"
     return 0
 }
-// If the extraction failed
+<# If the extraction failed #>
 else
 {
     echo "Error in extraction"
