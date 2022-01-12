2c75be01a500d263a5b5e4a96b60a3491fa0213d
Birce <birce.ertizman@uipath.com>
2022-01-12 14:03:11 +0100

    chore: extraction script venv update

--- scripts/transform.ps1
+++ scripts/transform.ps1
@@ -1,6 +1,5 @@
-$venvDir="YOUR_PYTHON_VIRTUAL_ENVIRONMENT_PATH"
-$appName="YOUR_DBT_PROJECT_PATH"
-
+$venvDir= $Env:DBT_PYT_DIR
+$transformDir = (get-item $PSScriptRoot).parent.FullName + '\transformations'
 <# Custom function for appending text to a file #>
 function Write-Log
 {
@@ -16,7 +15,7 @@ Param ([string]$cmdType)
     try
     {
         Write-Log ("dbt $cmdType START")
-        cmd /c "cd /d `"$venvDir\Scripts`" & activate & cd /d `"$Env:DBT_PROFILES_DIR\$appName`" & dbt $cmdType 1> `"$responseFile`" 2>&1"
+        cmd /c "cd /d `"$venvDir\Scripts`" & activate & cd /d `"$transformDir`" & dbt $cmdType 1> `"$responseFile`" 2>&1"
     }
     catch
     {
@@ -63,5 +62,6 @@ $scriptDir = $PSScriptRoot
 $Logfile = $scriptDir + "\LogFile.log"
 $responseFile=$scriptDir + "\response.txt"
 
+Execute-dbt("deps")
 Execute-dbt("run")
 Execute-dbt("test")
