64c99588502660eb062e0bd96a5a0bb7974966c4
Maraise <maraise.veens@uipath.com>
2022-01-19 16:51:09 +0100

    refactor: updated input tables to use renamed raw tables
    in snowflake and sql server tables were created consistent with the naming convention entity_raw. Updated files to use these renamed tables.

--- transformations/models/1_input/Purchase_orders_input.sql
+++ transformations/models/1_input/Purchase_orders_input.sql
@@ -1,16 +1,16 @@
-with Raw_purchase_orders as (
-    select * from {{ source(var("schema_sources"), 'Raw_purchase_orders') }}
+with Purchase_orders_raw as (
+    select * from {{ source(var("schema_sources"), 'Purchase_orders_raw') }}
 ),
 
 /* Input table for the purchase order entity containing purchase order properties. */
 Purchase_orders_input as (
     select
         -- Convert non-text fields to the correct data type.
-        {{ pm_utils.to_timestamp('Raw_purchase_orders."Created_at"') }} as "Created_at",
-        Raw_purchase_orders."Creator",
-        Raw_purchase_orders."ID",
-        {{ pm_utils.to_double('Raw_purchase_orders."Price"') }} as "Price"
-    from Raw_purchase_orders
+        {{ pm_utils.to_timestamp('Purchase_orders_raw."Created_at"') }} as "Created_at",
+        Purchase_orders_raw."Creator",
+        Purchase_orders_raw."ID",
+        {{ pm_utils.to_double('Purchase_orders_raw."Price"') }} as "Price"
+    from Purchase_orders_raw
 )
 
 select * from Purchase_orders_input
