53f3e1b6f6b1db6f37578b2362573577220cbb14
Maraise <maraise.veens@uipath.com>
2022-01-21 14:09:46 +0100

    refactor: removed renames to on-prem logic
    removed renames to on prem logic, all output attributes in the form of Case_ID, not Case ID. 
    updated tests accordingly

--- transformations/models/5_business_logic/Due_dates.sql
+++ transformations/models/5_business_logic/Due_dates.sql
@@ -29,10 +29,10 @@ Due_dates_preprocessing as (
 -- The fields on this table should match the data model.
 Due_dates as (
     select
-        Due_dates_preprocessing."Event_ID" as "Event ID",
-        Due_dates_preprocessing."Due_date" as "Due date",
-        Due_dates_preprocessing."Actual_date" as "Actual date",
-        Due_dates_preprocessing."Expected_date" as "Expected date"
+        Due_dates_preprocessing."Event_ID",
+        Due_dates_preprocessing."Due_date",
+        Due_dates_preprocessing."Actual_date",
+        Due_dates_preprocessing."Expected_date"
     from Due_dates_preprocessing
 )
 
