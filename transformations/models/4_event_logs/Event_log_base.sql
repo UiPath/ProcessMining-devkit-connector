46f99cd167ac5067e8a5a31429443b0e9b2462fe
Celine Verhoef <celine.verhoef@uipath.com>
2022-02-23 08:17:57 +0100

    feat: added automation estimates to the event log base

--- transformations/models/4_event_logs/Event_log_base.sql
+++ transformations/models/4_event_logs/Event_log_base.sql
@@ -2,6 +2,10 @@ with Purchase_order_event_log as (
     select * from {{ ref('Purchase_order_event_log') }}
 ),
 
+Automation_estimates as (
+    select * from {{ ref('Automation_estimates') }}
+),
+
 -- The fields on this table should match the data model.
 Event_log_base as (
     select
@@ -11,10 +15,14 @@ Event_log_base as (
         Purchase_order_event_log."Activity",
         Purchase_order_event_log."Event_end",
         -- Optional
+        Automation_estimates."Event_cost",
         Purchase_order_event_log."Event_detail",
+        Automation_estimates."Event_processing_time",
         Purchase_order_event_log."Team",
         Purchase_order_event_log."User"
     from Purchase_order_event_log
+    left join Automation_estimates
+        on Purchase_order_event_log."Activity" = Automation_estimates."Activity"
 )
 
 select * from Event_log_base
