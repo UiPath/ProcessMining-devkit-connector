b879a8584b319a7fcb1c660bbd60881ec2853949
Celine Verhoef <celine.verhoef@uipath.com>
2022-04-05 11:37:52 +0200

    chore: automation estimates input file for seeds

--- transformations/models/4_event_logs/Event_log_base.sql
+++ transformations/models/4_event_logs/Event_log_base.sql
@@ -2,8 +2,8 @@ with Purchase_order_event_log as (
     select * from {{ ref('Purchase_order_event_log') }}
 ),
 
-Automation_estimates as (
-    select * from {{ ref('Automation_estimates') }}
+Automation_estimates_input as (
+    select * from {{ ref('Automation_estimates_input') }}
 ),
 
 -- The fields on this table should match the data model.
@@ -15,14 +15,14 @@ Event_log_base as (
         Purchase_order_event_log."Activity",
         Purchase_order_event_log."Event_end",
         -- Optional
-        Automation_estimates."Event_cost",
+        Automation_estimates_input."Event_cost",
         Purchase_order_event_log."Event_detail",
-        Automation_estimates."Event_processing_time",
+        Automation_estimates_input."Event_processing_time",
         Purchase_order_event_log."Team",
         Purchase_order_event_log."User"
     from Purchase_order_event_log
-    left join Automation_estimates
-        on Purchase_order_event_log."Activity" = Automation_estimates."Activity"
+    left join Automation_estimates_input
+        on Purchase_order_event_log."Activity" = Automation_estimates_input."Activity"
 )
 
 select * from Event_log_base
