03e2ec6f464cfbe3827de4995df57f3d617b1ce9
Jasper Selman <jasper.selman@uipath.com>
2022-07-19 09:07:08 +0200

    chore: add errors for linter

--- transformations/models/1_input/Automation_estimates_input.sql
+++ transformations/models/1_input/Automation_estimates_input.sql
@@ -7,7 +7,7 @@ Automation_estimates_input as (
     select
         -- Convert all fields to the correct data type.
         {{ pm_utils.to_varchar('Automation_estimates."Activity"') }} as "Activity",
-        {{ pm_utils.to_double('Automation_estimates."Event_cost"') }} as "Event_cost",
+        {{ pm_utils.to_double('Automation_estimates."Event_cost"') }} as "Event_cost",    
         {{ pm_utils.to_integer('Automation_estimates."Event_processing_time"') }} as "Event_processing_time"
     from Automation_estimates
 )
--- transformations/models/1_input/Purchase_orders_status.sql
+++ transformations/models/1_input/Purchase_orders_status.sql
@@ -7,7 +7,7 @@ Purchase_orders_status as (
     select
         Purchase_orders_status_raw."ID",
         Purchase_orders_status_raw."Status"
-    from Purchase_orders_status_raw
+    from Purchase_orders_status_raw   
 )
 
 select * from Purchase_orders_status
