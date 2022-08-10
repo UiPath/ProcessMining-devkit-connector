4d3e332bb9773661bf5fe07b58e626749d86a8cc
Celine Verhoef <celine.verhoef@uipath.com>
2022-08-10 09:09:05 +0200

    refactor: add seeds example with activity configuration

--- transformations/models/4_event_logs/Event_log_base.sql
+++ transformations/models/4_event_logs/Event_log_base.sql
@@ -2,6 +2,10 @@ with Purchase_order_event_log as (
     select * from {{ ref('Purchase_order_event_log') }}
 ),
 
+Activity_configuration as (
+    select * from {{ ref('Activity_configuration') }}
+),
+
 -- The fields on this table should match the data model.
 Event_log_base as (
     select
@@ -11,10 +15,13 @@ Event_log_base as (
         Purchase_order_event_log."Activity",
         Purchase_order_event_log."Event_end",
         -- Optional
+        Activity_configuration."Activity_order",
         Purchase_order_event_log."Event_detail",
         Purchase_order_event_log."Team",
         Purchase_order_event_log."User"
     from Purchase_order_event_log
+    left join Activity_configuration
+        on Purchase_order_event_log."Activity" = Activity_configuration."Activity"
 )
 
 select * from Event_log_base
