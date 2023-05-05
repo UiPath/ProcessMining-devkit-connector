65ea9e2836d50accc3f7fbabd61b42bd6ff61d20
Celine Verhoef <celine.verhoef@uipath.com>
2023-05-05 15:20:03 +0200

    feat: implement mandatory macro

--- transformations/models/1_input/PO_approvals_input.sql
+++ transformations/models/1_input/PO_approvals_input.sql
@@ -2,19 +2,17 @@
     pre_hook="{{ pm_utils.create_index(source('sources', 'PO_approvals')) }}"
 ) }}
 
-with PO_approvals as (
-    select * from {{ source('sources', 'PO_approvals') }}
-),
+{% set source_table = source('sources', 'PO_approvals') %}
 
 /* Transaction log of purchase order approvals.
 The approved purchase order is identified by the ID. */
-PO_approvals_input as (
+with PO_approvals_input as (
     select
-        {{ pm_utils.to_timestamp('PO_approvals."Approved_at"') }} as "Approved_at",
-        {{ pm_utils.to_varchar('PO_approvals."Approved_by"') }} as "Approved_by",
-        {{ pm_utils.to_varchar('PO_approvals."ID"') }} as "ID",
-        {{ pm_utils.to_varchar('PO_approvals."Level"') }} as "Level"
-    from PO_approvals
+        {{ pm_utils.mandatory(source_table, '"Approved_at"', 'datetime') }} as "Approved_at",
+        {{ pm_utils.mandatory(source_table, '"Approved_by"') }} as "Approved_by",
+        {{ pm_utils.mandatory(source_table, '"ID"') }} as "ID",
+        {{ pm_utils.mandatory(source_table, '"Level"') }} as "Level"
+    from {{ source_table }}
 )
 
 select * from PO_approvals_input
--- transformations/models/1_input/Transaction_history_input.sql
+++ transformations/models/1_input/Transaction_history_input.sql
@@ -2,21 +2,19 @@
     pre_hook="{{ pm_utils.create_index(source('sources', 'Transaction_history')) }}"
 ) }}
 
-with Transaction_history as (
-    select * from {{ source('sources', 'Transaction_history') }}
-),
+{% set source_table = source('sources', 'Transaction_history') %}
 
 /* Transaction log describing changes on entities identified by the ID.
 The change is defined by the Field in combination with the New_value and Old_value. */
-Transaction_history_input as (
+with Transaction_history_input as (
     select
-        {{ pm_utils.to_varchar('Transaction_history."Field"') }} as "Field",
-        {{ pm_utils.to_varchar('Transaction_history."ID"') }} as "ID",
-        {{ pm_utils.to_varchar('Transaction_history."New_value"') }} as "New_value",
-        {{ pm_utils.to_varchar('Transaction_history."Old_value"') }} as "Old_value",
-        {{ pm_utils.to_timestamp('Transaction_history."Timestamp"') }} as "Timestamp",
-        {{ pm_utils.to_varchar('Transaction_history."User"') }} as "User"
-    from Transaction_history
+        {{ pm_utils.mandatory(source_table, '"Field"') }} as "Field",
+        {{ pm_utils.mandatory(source_table, '"ID"') }} as "ID",
+        {{ pm_utils.mandatory(source_table, '"New_value"') }} as "New_value",
+        {{ pm_utils.mandatory(source_table, '"Old_value"') }} as "Old_value",
+        {{ pm_utils.mandatory(source_table, '"Timestamp"', 'datetime') }} as "Timestamp",
+        {{ pm_utils.mandatory(source_table, '"User"') }} as "User"
+    from {{ source_table }}
 )
 
 select * from Transaction_history_input
